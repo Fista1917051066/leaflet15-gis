a31095061bf5e6f82a476c1217a9a56d423d3c1b
Fista1917051066 <fistadwi10@gmail.com>
2021-05-12 12:18:52 +0700

    leaflet

---
+++ index.php
@@ -0,0 +1,69 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"
+   integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A=="
+   crossorigin=""/>
+
+    <style>
+        #mapid { height: 100%; }
+    </style>
+
+    <title>Leaflet</title>
+</head>
+
+<body>
+    <div id="mapid"></div>
+</body>
+
+   <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"
+   integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA=="
+   crossorigin=""></script>
+   
+    <script>
+        var mymap = L.map('mapid').setView([-5.511652,104.461309], 13);
+    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw'
+}).addTo(mymap);
+
+var marker = L.marker([-5.511652,104.461309]).addTo(mymap)
+
+var circle = L.circle([-5.516948, 104.475014], {
+    color: 'red',
+    fillColor: '#f03',
+    fillOpacity: 0.5,
+    radius: 300
+}).addTo(mymap);
+
+var polygon = L.polygon([
+    [-5.529288, 104.451874],
+    [-5.492692, 104.414784],
+    [-5.522106, 104.387309],
+    [-5.571488, 104.415339]
+]).addTo(mymap);
+
+var popup = L.popup();
+
+function onMapClick(e) {
+    popup
+        .setLatLng(e.latlng)
+        .setContent("You clicked the map at " + e.latlng.toString())
+        .openOn(mymap);
+}
+
+mymap.on('click', onMapClick);
+
+marker.bindPopup("Masjid").openPopup();
+circle.bindPopup("Sukaraja");
+polygon.bindPopup("Hutan");
+
+    </script>
+
+</html>
